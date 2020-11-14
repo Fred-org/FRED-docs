7e8e6e9a51ede415515982e36ac82e9445242351
Angelo Schiavi <angelo.schiavi@uniroma1.it>
2020-11-14 19:31:15 +0100

    from version to version

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = 'FRED collaboration'
 author = 'FRED collaboration'
 
 # The full version, including alpha/beta/rc tags
-version = '3.50'
+version = '3.50 ⟶ 3.55'
 # The full version, including alpha/beta/rc tags.
 release = '3.50'
 
